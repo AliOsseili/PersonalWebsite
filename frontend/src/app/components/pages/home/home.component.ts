502d8bd9b70968fe11fb8205af3552624da2dccd
SlavKing <aliosseili2001@live.ca>
2023-05-17 09:03:54 -0400

    Capture input
    Capturing the input on the front end, and storing it.

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  name = '';
+  message = '';
 
+  getValueName(input: string) {
+    this.name = input;
+  }
+  getValueMessage(input: string) {
+    this.message = input;
+  }
+  sendMessage() {
+    console.log(this.name);
+    console.log(this.message);
+  }
 }
